a90d6929e2db3699e8b3ba15c09a392637f42834
Thomas Moon <tmoon@illinois.edu>
2022-03-22 14:32:14 -0500

    lec16

--- lec15/stick.c
+++ lec15/stick.c
@@ -3,11 +3,35 @@
 #define N 10
 #define M 3
 
-const int set[M] = {3,7,10};
+//const int set[M] = {3,7,10};
+const int set[M] = {10,7,3};
 // solution[N]: stores the solution
 // idx: index for the solution matrix
 // total: remaining length 
+//
+// total = 3
+// my first try = solution [0] = 3
+// recursive call solve(solution, 1, 0)
+//
 int solve(int solution[N], int idx,  int total){
+    int j;
+    // base case
+    if( total == 0 && idx <= N  ){
+        return 1;
+    }
+    // recursive
+    else{
+        for(j=0;j<M;j++){
+            if( total - set[j] >= 0    ){//safe move
+                solution[idx] = set[j]; // make decision
+                if( solve(solution, idx+1, total - set[j]) == 1)
+                    return 1;
+                else
+                    solution[idx] = 0;
+            }
+        }
+        return 0;
+    }
 }
 
 int main(){
