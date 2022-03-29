a1253fbb3796c7980590192090b3e2c9168d72eb
Thomas Moon <tmoon@illinois.edu>
2022-03-29 13:18:18 -0500

    lec18

---
+++ lec18/test.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <stdlib.h>  // <-- for malloc
+
+
+int main(){
+    int x = 10;
+    int *ptr; 
+    ptr  = &x; 
+    printf("%d\n", *ptr);
+    printf("%d\n", x);
+
+    ptr = (int*) malloc(sizeof(int));
+
+    *ptr = 5;
+    printf("%d\n", *ptr);
+
+    int *ptr2 = (int*) malloc(sizeof(int));
+
+    free(ptr);
+    ptr = ptr2;
+
+    free(ptr2);
+    //free(ptr); // double-free
+
+}
